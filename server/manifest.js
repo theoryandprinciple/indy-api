240641a3772939ca334254ab5a8013825601974f
Matt Boutet <matt@theoryandprinciple.com>
2021-01-22 12:17:41 -0500

    Minor cleanup, v bump

--- server/manifest.js
+++ server/manifest.js
@@ -110,7 +110,17 @@ module.exports = new Confidence.Store({
                         }
                     },
                     production: {
-                        migrateOnStart: false
+                        migrateOnStart: false,
+                        knex: {
+                            client: 'pg',
+                            useNullAsDefault: true,
+                            connection: {
+                                host: process.env.DB_HOST,
+                                user: process.env.DB_USER,
+                                password: process.env.DB_PASSWORD,
+                                database: process.env.DB_NAME
+                            }
+                        }
                     }
                 }
             },
